9817861a3334fc4b653dd0f03ebabae624af2dc1
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-04-30 18:36:17 -0300

    FIX: Correcoes em main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from tarefa import Tarefa
 # Criando as tarefas
 tarefa1 = Tarefa('Tarefa 1', 10)
 tarefa2 = Tarefa('Tarefa 2', 4)
-tarefa3 = Tarefa('Tarefa 1', 7)
-tarefa4 = Tarefa('Tarefa 2', 8)
+tarefa3 = Tarefa('Tarefa 3', 7)
+tarefa4 = Tarefa('Tarefa 4', 8)
 
 tarefas = [tarefa1, tarefa2, tarefa3, tarefa4]
 
