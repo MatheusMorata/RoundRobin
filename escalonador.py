1c039ceae300ce1949d8b0e15e66ebc7dcc0c7ec
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-04-30 16:48:04 -0300

    FEAT: Alteracoes em escalonador.py

--- escalonador.py
+++ escalonador.py
@@ -1,14 +1,26 @@
 import os
-import singal
+import signal
 from multiprocessing import Process
 from time import sleep
 
-
 class Escalonador:
-
-    def __init__(self,quantum, tarefas):
+    
+    def __init__(self, quantum, tarefas):
         self.quantum = quantum
         self.tarefas = tarefas
-        
-    def roundRobin():
-        
+
+    def roundRobin(self):
+        processos = []
+        for tarefa in self.tarefas:
+            # Criar um processo para cada tarefa
+            p = Process(target=tarefa.executar)
+            p.start()
+            processos.append(p)
+
+        while any(p.is_alive() for p in processos):
+            for p in processos:
+                if p.is_alive():
+                    os.kill(p.pid, signal.SIGSTOP)  # Pausa o processo
+                    sleep(self.quantum)
+                    os.kill(p.pid, signal.SIGCONT)  # Continua o processo
+                    sleep(1)  
