39595562da19046c98d713f48dba6fcbb430860e
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-04-30 16:49:46 -0300

    FEAT: Alteracoes em main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+# SIMULAÇÃO
 from escalonador import Escalonador
 from tarefa import Tarefa
 
-tarefa1 = Tarefa('teste', 12)
-tarefa2 = Tarefa('teste1', 15)
-Tarefas = [tarefa1, tarefa2]
+# Criando as tarefas
+tarefa1 = Tarefa('Tarefa 1', 10)
+tarefa2 = Tarefa('Tarefa 2', 4)
+tarefa3 = Tarefa('Tarefa 1', 7)
+tarefa4 = Tarefa('Tarefa 2', 8)
 
-e = Escalonador(12, Tarefas)
-e.roundRobin()
+tarefas = [tarefa1, tarefa2, tarefa3, tarefa4]
+
+# Criando o escalonador com quantum 4
+escalonador = Escalonador(4, tarefas)
+
+# Iniciando o algoritmo Round Robin
+escalonador.roundRobin()
