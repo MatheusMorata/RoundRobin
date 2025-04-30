5e667fd72b7d9c8deacdbec494bb388ee1c53d12
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-04-30 19:32:46 -0300

    FEAT: Mudancas tarefa.py

--- tarefa.py
+++ tarefa.py
@@ -7,6 +7,3 @@ class Tarefa:
 
     def executar(self):
         print(f"Executando tarefa: {self.nome}")
-        while self.tempo_restante > 0:
-            print(f"{self.nome}: {self.tempo_restante} unidades de tempo restantes.")
-            self.tempo_restante -= 1
