fc2f9c5e211ead341275427259f9b827ea630962
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-04-30 16:19:01 -0300

    FEAT: Alteracoes em escalonador.py

--- escalonador.py
+++ escalonador.py
@@ -1,16 +1,17 @@
 from multiprocessing import Process
 from time import sleep
+from queue import Queue
 
 class Escalonador:
 
     def __init__(self,quantum, tarefas):
         self.quantum = quantum
-        self.tarefas = tarefas
-        
+        self.fila = Queue()
+        for tarefa in self.tarefas:
+            self.fila.put(tarefa)
         
     def roundRobin():
-        for i in range(0, len(self.tarefas)):
-            print(self.tarefas[i].nome)
-            print(self.tarefas[i].duracao)
-            print("\n")
-            
+        while not self.fila.empty():
+            tarefa = self.fila.get()
+            if tarefa.executar(self.quantum):
+                self.fila.put(tarefa)
