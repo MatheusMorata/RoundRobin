f8503fdd4e700de155c40f61edc63c6eab8b1b60
Matheus <65503571+MatheusMorata@users.noreply.github.com>
2025-04-30 16:37:23 -0300

    REFECT: Alteracoes em escalonador.py

--- escalonador.py
+++ escalonador.py
@@ -1,17 +1,14 @@
+import os
+import singal
 from multiprocessing import Process
 from time import sleep
-from queue import Queue
+
 
 class Escalonador:
 
     def __init__(self,quantum, tarefas):
         self.quantum = quantum
-        self.fila = Queue()
-        for tarefa in self.tarefas:
-            self.fila.put(tarefa)
+        self.tarefas = tarefas
         
     def roundRobin():
-        while not self.fila.empty():
-            tarefa = self.fila.get()
-            if tarefa.executar(self.quantum):
-                self.fila.put(tarefa)
+        
